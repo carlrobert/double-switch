ed6f9f64308a55381db419d51122f62ae52d4ee3
Robert Storlind <robert.storlind@gmail.com>
2024-11-10 19:14:11 +0100

    Clean up unused sample code

--- main.ts
+++ main.ts
@@ -7,9 +7,11 @@ interface DoubleSwitchSettings {
 	myLightModeThemeName: string;
 }
 
+const NOSWITCH = "Don't switch";
+
 const DEFAULT_SETTINGS: DoubleSwitchSettings = {
-	myDarkModeThemeName: 'Blackbird',
-	myLightModeThemeName: 'Primary'
+	myDarkModeThemeName: NOSWITCH,
+	myLightModeThemeName: NOSWITCH
 }
 
 export default class DoubleSwitchPlugin extends Plugin {
@@ -19,58 +21,10 @@ export default class DoubleSwitchPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new MySettingTab(this.app, this));
 
+		// Core logic of the app
 		this.app.workspace.on("css-change", () => {
 			const darkModeNow = document.body.hasClass("theme-dark");
 			if (this.inDarkMode != darkModeNow) {
@@ -86,7 +40,7 @@ export default class DoubleSwitchPlugin extends Plugin {
 	}
 
 	setTheme(themeName: string) {
-		if (themeName !== "Don't switch") {
+		if (themeName !== NOSWITCH) {
 			//@ts-ignore
 			this.app.customCss.setTheme(themeName);
 		}
@@ -106,26 +60,11 @@ export default class DoubleSwitchPlugin extends Plugin {
 
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class MySettingTab extends PluginSettingTab {
 	plugin: DoubleSwitchPlugin;
 	DEFAULT_THEME_KEY = "";
-	DEFAULT_THEME_TEXT = "Don't switch";
+	DEFAULT_THEME_TEXT = "Default";
 
 	constructor(app: App, plugin: DoubleSwitchPlugin) {
 		super(app, plugin);
@@ -140,6 +79,7 @@ class SampleSettingTab extends PluginSettingTab {
 		new Setting(this.containerEl)
 			.setName('Theme to use with light mode').setDesc('Pick from installed themes')
 			.addDropdown(async dropdown => {
+				dropdown.addOption(NOSWITCH, NOSWITCH)
 				for (const key of Object.values(this.getThemes())) {
 					dropdown.addOption(key, this.getThemeNames(key));
 				}
@@ -153,6 +93,7 @@ class SampleSettingTab extends PluginSettingTab {
 		new Setting(this.containerEl)
 			.setName('Theme to use with dark mode').setDesc('Pick from installed themes')
 			.addDropdown(async dropdown => {
+				dropdown.addOption(NOSWITCH, NOSWITCH)
 				for (const key of Object.values(this.getThemes())) {
 					dropdown.addOption(key, this.getThemeNames(key));
 				}
@@ -177,6 +118,5 @@ class SampleSettingTab extends PluginSettingTab {
 			return item;
 		}
 	}
-
 
 }
